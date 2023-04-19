42bbf76a3e8f440416de1d35a8992179224a8cd4
CodeLearningSlayer <rubtsov.maks2015@yandex.ru>
2023-04-19 21:46:55 +0300

    functionality added

--- front/src/index.js
+++ front/src/index.js
@@ -5,6 +5,60 @@ import Embed from "@editorjs/embed"
 import ImageTool from '@editorjs/image';
 import SimpleImage from "@editorjs/simple-image";
 import "./css/style.scss";
+import notifier from "codex-notifier";
+import {ConfirmNotifierOptions, NotifierOptions, PromptNotifierOptions} from 'codex-notifier';
+
+class Pinner {
+    constructor(api){
+        this._pinned = {}
+        this._api = api;
+    }
+
+    getBlock() {
+        return this._block
+    }
+
+    getPinnedBlocks() {
+        this._api.configuration.data.blocks.forEach((block, index) => {
+            if (block.data.fixed === true){
+                this._pinned[index] = this._api.blocks.getBlockByIndex(index);
+            }
+        })
+    }
+
+    hideTunesBlock() {
+        const toolbar = document.querySelector(".ce-toolbar");
+        const allBlocks = Array.from(document.querySelectorAll(".ce-block"));
+        
+        document.querySelector(".codex-editor__redactor").addEventListener("mouseover", (e) => {
+            let flag = false;
+            allBlocks.forEach((item, index) => {
+                const isToolboxOpened = document.querySelector(".codex-editor--toolbox-opened") ?? false;
+                const isSelection = document.querySelector(".ce-block--selected") ?? false;
+                if (e.composedPath().indexOf(item) != -1 && index in Object.keys(this._pinned) && !isSelection && !isToolboxOpened){
+                    flag = true;
+                }
+            if (flag == true)
+                toolbar.classList.add("hidden");
+            else toolbar.classList.remove("hidden");
+        })
+        }, true)
+    }
+
+    logApi(){
+        console.log(this._api)
+    }
+    
+    getLastIndexOfPinnedBlocks(){
+        return Number(Object.keys(this._pinned).slice(-1));
+    }
+
+    getConfig () {
+        return this._block.config
+    }
+}
+
+let blockToPin;
 
 const editor = new EditorJS({
     holder: "editorjs",
@@ -41,24 +95,52 @@ const editor = new EditorJS({
             class: SimpleImage
         }
     },
+    hideToolbar:true,
     data: {
         time: 1552744582955,
         blocks: [
             {
                 type: "paragraph",
                 data: {
-                    text: "Моё тестовое задание"
-                }
+                    text: "Моё тестовое задание",
+                    fixed: true
+                },
             },
             {
                 type: "image",
                 data: {
-                  url: "https://picsum.photos/600"
-                }
-            }
+                  url: "https://picsum.photos/500",
+                  fixed: true
+                },
+            },
         ],
         version: "2.11.10" 
     },
+    onChange: (api, event) => {
+        const lastIndexOfPinnedBlocks = blockToPin.getLastIndexOfPinnedBlocks()
+        switch(event.type){
+            case 'block-added':
+                if (event.detail.index <= lastIndexOfPinnedBlocks){
+                    notifier.show({
+                        message:"Нельзя добавлять блоки выше закрепленных",
+                        style: "error" 
+                    })
+                    api.blocks.delete(event.detail.index);
+                }
+                break;
+            
+            case 'block-moved':
+                if (event.detail.toIndex == lastIndexOfPinnedBlocks){
+                    notifier.show({
+                        message:"Нельзя менять местами с закрепленными блоками",
+                        style: "error" 
+                    });
+                    api.blocks.move(lastIndexOfPinnedBlocks + 1);
+                }
+                break;
+        } 
+        console.log(event.type);
+    }
 });
 let saveBtn = document.querySelector(".save-button");
 saveBtn.addEventListener('click', function(){
@@ -69,4 +151,15 @@ saveBtn.addEventListener('click', function(){
     })
 })
 
-fetch("http://localhost:3010/").then().then(res => console.log(res.text));
+blockToPin = new Pinner(editor);
+
+editor.isReady
+    .then(() => {
+        blockToPin.getPinnedBlocks(); // так достучимся до проверки
+        blockToPin.hideTunesBlock();
+    })
+    .catch(error => {
+        console.log(error);
+    }) 
+
+// fetch("http://localhost:3010/").then().then(res => console.log(res.text));
